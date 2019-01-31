4c5062ee3281e77ac44c3f3797caf27b9e7c1dcd
Htoonlin <wizard.htoon@gmail.com>
2019-01-31 15:35:04 +0630

    Export/Import Controller

--- src/main/java/com/sdm/core/controller/ReadController.java
+++ src/main/java/com/sdm/core/controller/ReadController.java
@@ -1,6 +1,9 @@
 package com.sdm.core.controller;
 
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
 import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
 import com.sdm.core.exception.GeneralException;
@@ -10,12 +13,16 @@ import com.sdm.core.model.response.PaginationModel;
 import com.sdm.core.util.Globalizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +32,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public abstract class ReadController<T extends DefaultEntity, ID extends Serializable> {
@@ -32,6 +40,17 @@ public abstract class ReadController<T extends DefaultEntity, ID extends Seriali
     protected static final Logger logger = LoggerFactory.getLogger(ReadController.class);
 
     protected abstract JpaRepository<T, ID> getRepository();
+
+    protected Class<T> getEntityClass() {
+        ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
+        return (Class<T>) type.getActualTypeArguments()[0];
+    }
+
+    protected CsvMapper getCsvMapper() {
+        CsvMapper mapper = new CsvMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        return mapper;
+    }
 
     protected AuthInfo getCurrentUser() {
         return (AuthInfo) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -78,13 +97,33 @@ public abstract class ReadController<T extends DefaultEntity, ID extends Seriali
         return ResponseEntity.ok(entity);
     }
 
+    @GetMapping("/export")
+    ResponseEntity<Resource> exportData(@RequestParam(value = "page", defaultValue = "0") int pageId,
+                                        @RequestParam(value = "size", defaultValue = "10") int pageSize) {
+        try {
+            Page<T> paging = getRepository().findAll(this.buildPagination(pageId, pageSize, "id:DESC"));
+            byte[] data = this.getCsvMapper().writerWithSchemaFor(this.getEntityClass())
+                .with(CsvSchema.emptySchema().withHeader()).writeValueAsBytes(paging.getContent());
+            Resource resource = new ByteArrayResource(data);
+            String attachment = "attachment; filename=\"export_" +
+                Globalizer.getDateString("yyyyMMddHHmmss", new Date()) + ".csv\"";
+
+            return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("text/csv; charset=utf-8"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, attachment)
+                .body(resource);
+
+        } catch (Exception ex) {
+            logger.error(ex.getLocalizedMessage(), ex);
+            throw new GeneralException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage());
+        }
+    }
+
     @GetMapping("/struct")
     ResponseEntity getStructure() {
-        ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
-        Class<T> entityClass = (Class<T>) type.getActualTypeArguments()[0];
         JsonSchemaGenerator schemaGenerator = new JsonSchemaGenerator(Globalizer.jsonMapper());
         try {
-            JsonSchema schema = schemaGenerator.generateSchema(entityClass);
+            JsonSchema schema = schemaGenerator.generateSchema(this.getEntityClass());
             return ResponseEntity.ok(schema);
         } catch (JsonMappingException ex) {
             throw new GeneralException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage());
