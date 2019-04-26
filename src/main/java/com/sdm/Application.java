62e494d87ab804c91fdd4bc88394d691f5a268ea
Htoonlin <wizard.htoon@gmail.com>
2019-04-26 13:13:00 +0630

    Fixed maven depecrated warnings

--- src/main/java/com/sdm/Application.java
+++ src/main/java/com/sdm/Application.java
@@ -1,5 +1,6 @@
 package com.sdm;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,11 +9,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        builder.bannerMode(Banner.Mode.OFF);
         return builder.sources(Application.class);
     }
 }
