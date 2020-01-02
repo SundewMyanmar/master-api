60d70dafb8f3bc7ffe4c386443939d8d570b95e4
Htoonlin <wizard.htoon@gmail.com>
2020-01-02 18:52:08 +0630

    Fixed imports

--- src/main/java/com/sdm/master/controller/RoleController.java
+++ src/main/java/com/sdm/master/controller/RoleController.java
@@ -1,12 +1,18 @@
 package com.sdm.master.controller;
 
 import com.sdm.core.controller.ReadWriteController;
+import com.sdm.core.model.response.ListModel;
 import com.sdm.core.repository.DefaultRepository;
 import com.sdm.master.entity.RoleEntity;
 import com.sdm.master.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/roles")
