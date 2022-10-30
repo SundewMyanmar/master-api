4e4fe12af1334ba7075a72391ba9a452862a4fdf
Htoonlin <wizard.htoon@gmail.com>
2022-10-30 21:45:33 +0630

    Save User Note

--- src/main/java/com/sdm/auth/controller/ProfileController.java
+++ src/main/java/com/sdm/auth/controller/ProfileController.java
@@ -45,138 +45,140 @@ import javax.validation.Valid;
 @RequestMapping("/me")
 public class ProfileController extends DefaultController {
 
-    public enum LINK_TYPE {
-        GOOGLE,
-        FACEBOOK
-    }
+	public enum LINK_TYPE {
+		GOOGLE,
+		FACEBOOK
+	}
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private TokenRepository tokenRepository;
+	@Autowired
+	private TokenRepository tokenRepository;
 
-    @Autowired
-    private SecurityManager securityManager;
+	@Autowired
+	private SecurityManager securityManager;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private AuthService authService;
+	@Autowired
+	private AuthService authService;
 
-    @Autowired
-    private GoogleAuthService googleAuthService;
+	@Autowired
+	private GoogleAuthService googleAuthService;
 
-    @Autowired
-    private FacebookAuthService facebookAuthService;
+	@Autowired
+	private FacebookAuthService facebookAuthService;
 
-    @Autowired
-    private JwtService jwtService;
+	@Autowired
+	private JwtService jwtService;
 
-    @Autowired
-    private BarCodeManager barCodeManager;
+	@Autowired
+	private BarCodeManager barCodeManager;
 
-    @Autowired
-    private FileService fileService;
+	@Autowired
+	private FileService fileService;
 
-    private User checkMe() {
-        return userRepository.findById(getCurrentUser().getUserId())
-                .orElseThrow(() -> new GeneralException(HttpStatus.BAD_REQUEST, localeManager.getMessage("invalid-user-account")));
-    }
+	private User checkMe() {
+		return userRepository.findById(getCurrentUser().getUserId())
+				.orElseThrow(() -> new GeneralException(HttpStatus.BAD_REQUEST,
+						localeManager.getMessage("invalid-user-account")));
+	}
 
-    @GetMapping("")
-    public ResponseEntity<User> getProfile() {
-        User user = this.checkMe();
-        return ResponseEntity.ok(user);
-    }
+	@GetMapping("")
+	public ResponseEntity<User> getProfile() {
+		User user = this.checkMe();
+		return ResponseEntity.ok(user);
+	}
 
-    @PostMapping("")
-    public ResponseEntity<User> updateProfile(@RequestBody User user) {
-        User existUser = this.checkMe();
+	@PostMapping("")
+	public ResponseEntity<User> updateProfile(@RequestBody User user) {
+		User existUser = this.checkMe();
 
-        existUser.setProfileImage(user.getProfileImage());
-        existUser.setDisplayName(user.getDisplayName());
-        existUser.setExtras(user.getExtras());
-        existUser = userRepository.save(existUser);
+		existUser.setProfileImage(user.getProfileImage());
+		existUser.setDisplayName(user.getDisplayName());
+		existUser.setNote(user.getNote());
+		existUser.setExtras(user.getExtras());
+		existUser = userRepository.save(existUser);
 
-        return ResponseEntity.ok(existUser);
-    }
+		return ResponseEntity.ok(existUser);
+	}
 
+	@PostMapping(value = "/changeProfileImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<User> uploadFile(@RequestParam("profileImage") List<MultipartFile> files) {
+		User existUser = this.checkMe();
 
-    @PostMapping(value = "/changeProfileImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<User> uploadFile(@RequestParam("profileImage") List<MultipartFile> files) {
-        User existUser = this.checkMe();
+		Field profileImageField = null;
+		FileClassification annotation = null;
+		try {
+			profileImageField = User.class.getDeclaredField("profileImage");
+			annotation = profileImageField.getAnnotation(FileClassification.class);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		}
+		if (files.size() > 0) {
+			File file = fileService.create(files.get(0), null, annotation);
+			existUser.setProfileImage(file);
+			userRepository.save(existUser);
+		}
+		return new ResponseEntity<User>(existUser, HttpStatus.OK);
+	}
 
-        Field profileImageField = null;
-        FileClassification annotation = null;
-        try {
-            profileImageField = User.class.getDeclaredField("profileImage");
-            annotation = profileImageField.getAnnotation(FileClassification.class);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
-        if (files.size() > 0) {
-            File file = fileService.create(files.get(0), null, annotation);
-            existUser.setProfileImage(file);
-            userRepository.save(existUser);
-        }
-        return new ResponseEntity<User>(existUser, HttpStatus.OK);
-    }
+	@PostMapping("/changePassword")
+	public ResponseEntity<User> changePassword(@Valid @RequestBody ChangePasswordRequest request) {
+		String phoneOrEmail = request.getUser();
+		if (Globalizer.isPhoneNo(phoneOrEmail))
+			phoneOrEmail = Globalizer.cleanPhoneNo(phoneOrEmail);
 
-    @PostMapping("/changePassword")
-    public ResponseEntity<User> changePassword(@Valid @RequestBody ChangePasswordRequest request) {
-        String phoneOrEmail = request.getUser();
-        if (Globalizer.isPhoneNo(phoneOrEmail))
-            phoneOrEmail = Globalizer.cleanPhoneNo(phoneOrEmail);
+		User user = userRepository.findFirstByPhoneNumberOrEmail(phoneOrEmail, phoneOrEmail).orElseThrow(
+				() -> new GeneralException(HttpStatus.BAD_REQUEST, localeManager.getMessage("invalid-user-account")));
 
-        User user = userRepository.findFirstByPhoneNumberOrEmail(phoneOrEmail, phoneOrEmail).orElseThrow(
-                () -> new GeneralException(HttpStatus.BAD_REQUEST, localeManager.getMessage("invalid-user-account")));
+		if (!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
+			throw new GeneralException(HttpStatus.BAD_REQUEST,
+					localeManager.getMessage("invalid-user-or-old-password"));
+		}
 
-        if (!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
-            throw new GeneralException(HttpStatus.BAD_REQUEST,
-                    localeManager.getMessage("invalid-user-or-old-password"));
-        }
+		String newPassword = passwordEncoder.encode(request.getNewPassword());
+		user.setPassword(newPassword);
+		userRepository.save(user);
 
-        String newPassword = passwordEncoder.encode(request.getNewPassword());
-        user.setPassword(newPassword);
-        userRepository.save(user);
+		return ResponseEntity.ok(user);
+	}
 
-        return ResponseEntity.ok(user);
-    }
+	@PostMapping("/refreshToken")
+	public ResponseEntity<User> refreshToken(@Valid @RequestBody TokenInfo tokenInfo,
+			HttpServletRequest servletRequest) {
+		User existUser = this.checkMe();
 
-    @PostMapping("/refreshToken")
-    public ResponseEntity<User> refreshToken(@Valid @RequestBody TokenInfo tokenInfo, HttpServletRequest servletRequest) {
-        User existUser = this.checkMe();
+		String currentToken = jwtService.createToken(existUser, tokenInfo, servletRequest);
+		existUser.setCurrentToken(currentToken);
 
-        String currentToken = jwtService.createToken(existUser, tokenInfo, servletRequest);
-        existUser.setCurrentToken(currentToken);
+		return ResponseEntity.ok(existUser);
+	}
 
-        return ResponseEntity.ok(existUser);
-    }
+	@DeleteMapping("/cleanToken")
+	public ResponseEntity<MessageResponse> cleanToken() {
+		AuthRequest request = new AuthRequest();
+		request.setDeviceId(getCurrentUser().getDeviceId());
+		request.setDeviceOS(getCurrentUser().getDeviceOs());
+		tokenRepository.cleanTokenByUserId(this.getCurrentUser().getUserId());
+		MessageResponse message = new MessageResponse(HttpStatus.OK, localeManager.getMessage("remove-success"),
+				localeManager.getMessage("clear-all-auth-token", this.getCurrentUser().getUserId()), null);
+		return ResponseEntity.ok(message);
+	}
 
-    @DeleteMapping("/cleanToken")
-    public ResponseEntity<MessageResponse> cleanToken() {
-        AuthRequest request = new AuthRequest();
-        request.setDeviceId(getCurrentUser().getDeviceId());
-        request.setDeviceOS(getCurrentUser().getDeviceOs());
-        tokenRepository.cleanTokenByUserId(this.getCurrentUser().getUserId());
-        MessageResponse message = new MessageResponse(HttpStatus.OK, localeManager.getMessage("remove-success"),
-                localeManager.getMessage("clear-all-auth-token", this.getCurrentUser().getUserId()), null);
-        return ResponseEntity.ok(message);
-    }
+	@GetMapping("/linkOAuth2/{type}")
+	public ResponseEntity<User> linkOAuth2(@PathVariable(value = "type") LINK_TYPE type,
+			@RequestParam(value = "accessId", defaultValue = "") String accessId) {
+		User user = this.checkMe();
 
-    @GetMapping("/linkOAuth2/{type}")
-    public ResponseEntity<User> linkOAuth2(@PathVariable(value = "type") LINK_TYPE type,
-                                           @RequestParam(value = "accessId", defaultValue = "") String accessId) {
-        User user = this.checkMe();
+		if (type.equals(LINK_TYPE.GOOGLE)) {
+			return googleAuthService.link(accessId, user);
+		} else if (type.equals(LINK_TYPE.FACEBOOK)) {
+			return facebookAuthService.link(accessId, user);
+		}
 
-        if (type.equals(LINK_TYPE.GOOGLE)) {
-            return googleAuthService.link(accessId, user);
-        } else if (type.equals(LINK_TYPE.FACEBOOK)) {
-            return facebookAuthService.link(accessId, user);
-        }
-
-        throw new GeneralException(HttpStatus.BAD_REQUEST, localeManager.getMessage("invalid-auth-linked-type"));
-    }
+		throw new GeneralException(HttpStatus.BAD_REQUEST, localeManager.getMessage("invalid-auth-linked-type"));
+	}
 }
